3828c65bb8655810d1742a55815d1946add3f6bf
DanishScout <kristianjuul1999@gmail.com>
2026-09-25 14:16:34 +0200

    Update global.js

--- frontend/global.js
+++ frontend/global.js
@@ -4,7 +4,7 @@
 
 const API_BASE_URL = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost' 
     ? 'http://127.0.0.1:8000' 
-    : 'https://vercel-git-306266126462.europe-west1.run.app';
+    : 'https://vercel-nine-opal-71.vercel.app/';
 let pizzaChartInstance = null;
 
 const minimalSaaSStyles = `
